ecba5fb3be5d236c336fe4b90dac504077ea1d04
KennyLeeCode <kenny.tlee04@gmail.com>
2025-11-24 20:07:40 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,20 +5,17 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import movieRoutes from './routes/movieRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-<<<<<<< HEAD
 import genreRoutes from './routes/genreRoutes.js'
 import reviewRoutes from './routes/reviewRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import watchlistRoutes from './routes/watchlistRoutes.js'
 
 const swaggerDocument = YAML.load('./openapi.yaml');
-=======
 import reviewRoutes from './routes/reviewRoutes.js'
 import watchlistRoutes from './routes/watchlistRoutes.js'
 
 
 const swaggerDocument = YAML.load('./docs/openapi.yaml');
->>>>>>> origin/main
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -39,12 +36,10 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // This all the movies api are
 app.use('/movies', movieRoutes);
 app.use('/auth', authRoutes);
-<<<<<<< HEAD
 app.use('/genres', genreRoutes);
 app.use('/reviews', reviewRoutes);
 app.use('/users', userRoutes);
 app.use('/watchlist', watchlistRoutes);
-=======
 
 app.use('/users',userRoutes);
 
@@ -52,8 +47,6 @@ app.use('/reviews', reviewRoutes);
 
 // need to make sure the user are only ones that can change the watchlist
 app.use('/watchlists', watchlistRoutes);
-
->>>>>>> origin/main
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
